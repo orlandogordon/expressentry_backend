9b0131fd95a2d8cab76da6952dbcace28e3acdf9
Orlando Gordon <ogordon1998@gmail.com>
2023-08-02 06:47:39 -0400

    Updated settings to serve static files from s3 bucket

--- expressentry_backend/settings.py
+++ expressentry_backend/settings.py
@@ -149,7 +149,7 @@ AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 DEFAULT_FILE_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
-# STATICFILES_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Default primary key field type
