144e700022688424af8428fa962243bc95340fed
Orlando Gordon <ogordon1998@gmail.com>
2023-11-14 15:13:09 -0500

    Connect to new postgresql database. The previous database had expired.

--- expressentry_backend/settings.py
+++ expressentry_backend/settings.py
@@ -148,9 +148,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-DEFAULT_FILE_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
-AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -167,4 +167,3 @@ INTERNAL_IPS = [
 ]
 
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
-
